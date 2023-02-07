4058a52db5e37b8a36b6c78d18c7cd0f5f883976
Tomke <pfoch.tomke@gmail.com>
2023-02-07 16:08:20 +0100

    API change

--- Conscreen_ANSI.h
+++ Conscreen_ANSI.h
@@ -9,7 +9,7 @@ typedef struct
         uint8_t r,g,b;
 } Conscreen_color;
 
-#define CONSCREEN_ANSI_COLOR(R,G,B) (Conscreen_color){R,G,B}
+#define CONSCREEN_RGRGB(R,G,B) (Conscreen_color){R,G,B}
 
 
 typedef struct
